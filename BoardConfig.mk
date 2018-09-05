671b980f56f030a93d8662d367466692ca43b7a2
Prashanth <me@prashanthca.in>
2018-09-06 00:22:06 +0530

    Init repo

---
+++ BoardConfig.mk
@@ -0,0 +1,83 @@
+#
+# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/xolo/era_3
+
+TARGET_BOARD_PLATFORM := mt6737m
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := era_3
+
+
+TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := unknown
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_BOOTLOADER_BOARD_NAME := era_3
+
+BOARD_KERNEL_IMAGE_NAME := kernel
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,32N2 androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x04000000 --tags_offset 0x0e000000
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 3670016 #0x00380000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 4718592 #0x00480000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 147193856 #0x08c60000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 274464768 #0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+# BOARD_RECOVERYIMAGE_PARTITION_SIZE := 11260160
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/fstab.mt6735
+BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_RECOVERY_SWIPE := true
+BOARD_USES_MMCUTILS := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_INCLUDE_CRYPTO := true
+
+# TWRP
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_INJECTTWRP := false
+TW_DEVICE_VERSION := Xolo
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_INCLUDE_FB2PNG := true
+TW_HAS_DOWNLOAD_MODE := false
+INTERNAL_LOCAL_CLANG_EXCEPTION_PROJECTS := external/busybox/
+TW_NO_SCREEN_BLANK := true
+TW_CUSTOM_BATTERY_PATH := "/sys/devices/platform/battery/power_supply/battery"
+RECOVERY_SDCARD_ON_DATA := true
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery.fstab:recovery/root/etc/recovery.fstab
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab.mt6735:recovery/root/etc/fstab.mt6735
